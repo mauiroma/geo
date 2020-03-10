91731e9deea218e6cd5fc2521ca43831b5ee98eb
sforzapi <pierluigi.sforza@posteitaliane.it>
2020-03-10 12:50:08 +0100

    refactoring

--- run-docker-local.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t cqrs/bank-service:0.0.1 --no-cache . 
-docker run -p 8080:8080 cqrs/bank-service:0.0.1
--- test-api.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while true
-do
-  curl -v -X GET http://localhost:8081/api/v1/accounts
-    curl -X POST -H "Content-Type: application/json" -d '{"name":"John Doe", "email":"john.doe@google.com"}' "http://localhost:8081/api/v1/create-account"
-done
