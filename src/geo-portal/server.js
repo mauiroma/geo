07c8d9e315fb53d5a77870977bb5079a23baa27b
Maurizio Romani <maurizio.romani@gmail.com>
2020-03-16 19:03:19 +0100

    Fix some issues

--- src/geo-portal/server.js
+++ src/geo-portal/server.js
@@ -4,8 +4,6 @@
 var http         =   require('http');
 var serverPort   =   process.env.MY_SERVER_PORT      || 8080;
 
-var backendURL   =   process.env.BACKEND_URL;
-
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World!!!\n");
@@ -13,4 +11,3 @@ var server = http.createServer(function (request, response) {
 
 server.listen(serverPort);
 console.log("Server running port: " + serverPort);
-console.log("Backend URL: " + backendURL);
