804d8ae52649ea34dd457f15d0a5cff8526f1431
Maurizio Romani <maurizio.romani@gmail.com>
2020-03-16 17:43:25 +0100

    Add bakend url as variable

--- src/geo-portal/server.js
+++ src/geo-portal/server.js
@@ -4,7 +4,7 @@
 var http         =   require('http');
 var serverPort   =   process.env.MY_SERVER_PORT      || 8080;
 
-var backendURL   =   process.env.BACKEND_URL
+var backendURL   =   process.env.BACKEND_URL;
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
@@ -13,3 +13,4 @@ var server = http.createServer(function (request, response) {
 
 server.listen(serverPort);
 console.log("Server running port: " + serverPort);
+console.log("Backend URL: " + backendURL);
